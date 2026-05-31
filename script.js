5d05959c9af70baa9d086db4b0e6c5f632c7f18f
GlackHits <07.alexacosta@gmail.com>
2026-05-31 15:55:41 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
 
-  const pagarBtn = document.getElementById("pagarBtn");
 
-  // 🔐 Lista de códigos y sus links
-  const codigos = {
-    "Menus2026": "https://buy.stripe.com/6oU6oJ3v6cAU39Y9Oz18c06",
-    "Premium2026": "https://buy.stripe.com/TU_LINK_PREMIUM",
-    "Basico2026": "https://buy.stripe.com/TU_LINK_BASICO",
-    "ClienteVIP": "https://buy.stripe.com/TU_LINK_VIP"
-  };
-
-  pagarBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const codigo = prompt("Ingresa tu código de acceso:");
-
-    if (codigo === null) return; // si cancela
-
-    if (codigos[codigo]) {
-      window.open(codigos[codigo], "_blank");
-    } else {
-      alert("Código incorrecto ❌");
-    }
-
-  });
-
-});
+const paymentCard = document.getElementById("paymentCard");
+function mostrarPago(){
+  paymentCard.classList.add("active");
+}
+function cerrarPago(){
+  paymentCard.classList.remove("active");
+}
